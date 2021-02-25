34874f83c4919cf2316cc7ddce3516d756c78034
Vincent Emonet <vincent.emonet@gmail.com>
2021-02-25 14:15:03 +0100

    fix update-sio.sh to pull ontologies before loading them

--- update-sio.sh
+++ update-sio.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+# Update sio ontology
+cd /data/semanticscience-data/semanticscience
+git pull
+
+# Update semanticchemistry ontology
+cd /data/semanticscience-data/semanticchemistry
+git pull
+
+# Load ontologies in semanticscience Virtuoso
 docker exec -it semanticscience-website php /loader/loadsio.php
